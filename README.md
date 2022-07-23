09cb40513dd9f4cb63d911e8ec08c8c26b3c9dcd
Yogeshwaran Arumugam <yogeshyoge739@gmail.com>
2022-07-23 19:45:58 +0530

    Added colors 2d Game Board

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Team-Akatsuki
-Hackathon - Unom
+<h1>Team Akatsuki</h1>
+<h4>Yogesh branch</h4>
+<ul>
+    <li><strong>app.py</strong></li>
+    <ul>
+        <li>Change the found0.json address to yours</li>
+        <li>display method diplays when -> No parameter = no grid || any parameter = with grid</li>
+    </ul>
+    <li>found0.json</li>
+    <ul>
+        <li>Initial State of the game</li>
+    </ul>
+    <li>colorCodes.py</li>
+    <ul>
+        <li>Viws all the color codes</li>
+        <li>bg func - background Color</li>
+        <li>fg func - Font Color</li>
+    </ul>
+</ul>
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import json
+from tabulate import tabulate
+
+
+def fg(text, color): return "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+def bg(text, color): return "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+
 
 class GameBoard:
     def __init__(self, path):
         self.gameJson = self.getJson(path)
         self.Board = self.initialBoard()
+        self.colCode = {
+            'w': bg('  ', 88),
+            'o': bg('  ', 11),
+            'm': bg('  ', 243),
+            'a': 226,
+            'b': 199
+        }
         self.makeBoard()
 
     def getJson(self, p):
@@ -13,33 +26,37 @@ class GameBoard:
     def initialBoard(self):
         board = list()
         for _ in range(15):
-            a = list('.' * 15)
+            a = ['  ' for i in range(15)]
             board.append(a)
         return board
-    
-    def display(self):
-        for x in self.Board:
-            for j in x:
-                print(f'| {j} ', end='')
-            print()
+
+    def display(self, g=False):
+        if g:
+            print(tabulate(self.Board, tablefmt='fancy_grid'))
+        else:
+            for x in self.Board:
+                for y in x:
+                    print(y, end='')
+                print()
 
     def fillUnits(self):
         for i in self.gameJson['units']:
             x, y = i['coordinates']
-            self.Board[x][y] = i['unit_id']
+            self.Board[x][y] = fg(' {}'.format(
+                i['unit_id']), self.colCode[i['agent_id']])
 
     def fillObs(self):
         for i in self.gameJson['entities']:
             x, y = i['x'], i['y']
-            self.Board[x][y] = i['type']
+            self.Board[x][y] = self.colCode[i['type']]
 
     def makeBoard(self):
         self.fillUnits()
         self.fillObs()
-        self.display()
 
     def getBoard(self):
         return self.Board
 
 
-newGame = GameBoard('found0.json')
+newGame = GameBoard('F:/Akatsuki/Team-Akatsuki/found0.json')
+newGame.display()
