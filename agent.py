f40b1d71f0850901164d230f57956a3b7b5a1fa9
hariharan141200 <51135395+hariharan1412@users.noreply.github.com>
2022-08-15 12:57:08 +0530

    Structured V2

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
-from locale import currency
+#I'M TOO FAR YOUNG TO WORRY ABOUT MY FAILURES.
+
 from typing import Union
 from game_state import GameState
 import asyncio
@@ -12,169 +13,150 @@ uri = os.environ.get(
 
 actions = ["up", "down", "left", "right", "bomb", "detonate"]
 
+
 class node:
-    def __init__(self , row , col , obj_type):
-        self.row = row 
-        self.col = col 
-        self.type = obj_type 
+    
+    def __init__(self , row , col):
+
+        self.row = row
+        self.col = col
+
+        self.obj_type = '.' #Type of This node i.e ENTITY type (Refer line : 53)
+    
+        self.id = self.col + self.row * 15
+
+        self.neibours = []
+
+    def score_reset(self):
 
         self.h_score = 0
         self.g_score = float("inf")
         self.f_score = float("inf")
 
-        self.id = self.col + self.row * 15
-
-        self.neibours = []
-
-    def add_neibour(self , grid):
-        # print("in add neibour")
-    
-        if self.row > 0 and grid[self.row - 1][self.col].type == 'free': #DOWN
-            # print("DOWN" , self.row , self.col , grid[self.row - 1][self.col].type ,  grid[self.row - 1][self.col].id )
+    def add_neibour(self , grid ):
+         
+        if self.row > 0 and grid[self.row - 1][self.col].obj_type == '.': #UP
             self.neibours.append(grid[self.row - 1][self.col])
 
-        if self.row < 14 and grid[self.row + 1][self.col].type == 'free': #UP
-            # print("UP" , self.row , self.col , grid[self.row + 1][self.col].type ,  grid[self.row + 1][self.col].id)
+        if self.row < 14 and grid[self.row + 1][self.col].obj_type == '.': #DOWN
             self.neibours.append(grid[self.row + 1][self.col])
 
-        if self.col < 14 and grid[self.row][self.col + 1].type == 'free': #RIGHT
-            # print("RIGHT" , self.row , self.col , grid[self.row][self.col + 1].type ,  grid[self.row][self.col + 1].id)
+        if self.col > 0 and grid[self.row ][self.col - 1].obj_type == '.': #LEFT
+            self.neibours.append(grid[self.row][self.col - 1])
+        
+        if self.col < 14 and grid[self.row][self.col + 1].obj_type == '.': #RIGHT
             self.neibours.append(grid[self.row][self.col + 1])
 
-        if self.col > 0 and grid[self.row ][self.col - 1].type == 'free': #LEFT
-            # print("LEFT" , self.row , self.col ,  grid[self.row ][self.col - 1].type , grid[self.row ][self.col - 1].id)
-            self.neibours.append(grid[self.row][self.col - 1])
+class Gameboard:
 
-class board():
-    def __init__(self , gameState , my_units , unit):
-        
+    def __init__(self): 
+
+        self.grid = [[node(i , j) for j in range(15)] for i in range(15)] #i row , j col
+
+        self.maja = lambda x : [14-x[1] , x[0]] #MAJA FUNCTION i.e is convert col , row to row , col (want to know reason call us :)
+
+        self.h = lambda start , end : abs(start.row - end.row) + abs(start.col - end.col) #Herustic function
+
+
+    def update_board(self , gameState): #Everytime the board get updated with current values 
+
         self.gameState = gameState 
-        self.units = my_units #D , F , H
-        self.unit = unit
-        self.start_pos = self.gameState['unit_state'][self.unit]['coordinates']
-        # self.end = None   #END POINT TO REACH
-
-        self.obstacle = []
-
-        for i in range(len(self.gameState['entities'])):
-            self.obstacle.append([self.gameState['entities'][i]['x'] , self.gameState['entities'][i]['y']])
-
-        #OPPONENT TEAM
-        self.obstacle.append(self.gameState['unit_state']['c']['coordinates'])
-        self.obstacle.append(self.gameState['unit_state']['e']['coordinates'])
-        self.obstacle.append(self.gameState['unit_state']['g']['coordinates'])
         
-        #SAME TEAM 
-        print(self.unit , self.units)
-        # self.units.remove(self.unit)
-        for i in self.units:
-            
-            if i != self.unit:
-                self.obstacle.append(self.gameState['unit_state'][i]['coordinates'])
-            
-
-        # self.obstacle.append(self.gameState['unit_state']['f']['coordinates'])
-        # self.obstacle.append(self.gameState['unit_state']['h']['coordinates'])
-
-        # self.h = lambda start , end : abs(start.row - end.row) + abs(start.col - end.col)
-        # print(self.obstacle)
-        
-        self.grid = [['' for i in range(15)] for j in range(15)] #INITIALIZE EMPTY GRID
-
-        for i in range(14 , -1 , -1):
-            # for j in range(14 , -1 , -1):
+        for i in range(15):
             for j in range(15):
-                if [i , j] in self.obstacle:
-                    obj_type = 'obs'
-                else:
-                    obj_type = 'free'
-                self.grid[i][j] = node(i , j , obj_type)
-        
-        for i in range(14 , -1 , -1):
-            # for j in range(14 , -1 , -1):
+                self.grid[j][i].obj_type = '.' #MAJA FUNCTION i.e is convert col , row to row , col
+
+        #ENTITIES i.e WOOD , STONE , METAL , BOMB , AMMO , FIRE , POWERUP => w , o , m , b , a , x , bp
+        for i in self.gameState['entities']:
+            self.grid[14-i['y']][i['x']].obj_type = i['type'] #MAJA FUNCTION i.e is convert col , row to row , col
+
+        # BOTs
+        # OPPONENT TEAM => c , e , g 
+        # OUR TEAM      => d , f , h 
+        for i in 'cdefgh':
+            x = self.maja(self.gameState['unit_state'][i]['coordinates'])
+            self.grid[x[0]][x[1]].obj_type = i
+
+
+        for i in range(15):
             for j in range(15):
-                self.grid[i][j].add_neibour(self.grid)
 
+                print(self.grid[i][j].obj_type , end=' ')
+            print()
 
-        self.start = self.grid[self.start_pos[0]][self.start_pos[1]]
-        self.end   = self.grid[7][7]
-
-        # print(self.start.row , self.start.col , self.start.id , self.start.type)
-
-    def h(self ,  start , end):
-        return abs(start.row - end.row) + abs(start.col - end.col)
 
     def take_action(self , came_from , current):
         
-        print("in take action")
+        path = []
         while current in came_from:
             current = came_from[current]
-
+    
             if current != self.start:
-                n = current
-            # n = current
-        # actions = ["up", "down", "left", "right", "bomb", "detonate"]
+                path.append(current)
         
+        path.insert(0 , self.end)
+        n = path[len(path)-1]
+        print(n.row , n.col)
+
         try:
-            print(n.row , n.col , self.start.row , self.start.col , self.unit)
-            
+            print(n.row , n.col)
+                  
             if n.col > self.start.col: 
-                print("UP")
-                return actions[0]
+                # print("RIGTH")
+                return actions[3]
+
             elif n.col < self.start.col:
-                print("DOWN")
-                return actions[1]
+                # print("LEFT")
+                return actions[2]
 
             elif n.row > self.start.row: 
-                print("RIGHT")
-                return actions[3]
+                # print("DOWN")
+                return actions[1]
+
             elif n.row < self.start.row: 
-                print("LEFT")
-                return actions[2]
+                # print("UP")
+                return actions[0]
         except:
             pass
 
-    def path_finding(self):
-        
+    def path_finding(self , unit , end): 
+
+        start_pos = self.maja(self.gameState['unit_state'][unit]['coordinates']) #UNIT postion #if not work remember to remove him from node
+
+        self.start = self.grid[start_pos[0]][start_pos[1]]
+
+        # self.end = self.grid[0][7]
+        self.end = self.grid[end[0]][end[1]]
+
+        for i in range(15):
+            for j in range(15):              
+                self.grid[i][j].score_reset()           
+
         self.path = {}
 
-        # self.open_set = []
         count = 0
         self.open_set = PriorityQueue()
 
-        # self.open_set.append(self.start)
         self.open_set.put((count , self.start))
 
         self.start.f_score = self.h(self.start , self.end)
         self.start.g_score = 0
 
         open_set_hash = {self.start}
-        # while self.open_set:
         while not self.open_set.empty():
 
-            
-            # current = self.open_set[0]
-            # for i in self.open_set:
-                # if i.f_score < current.f_score:
-                    # current = i 
             current = self.open_set.get()[1]
             open_set_hash.remove(current)
 
-            # print("current : " , current.id )
-            # print("current neibour : " , current.neibours )
-            # for i in current.neibours:
-            #     print(current.id , "NEIBOUR:" , i.id)
-            
+            current.add_neibour(self.grid) 
+
             if current == self.end:
                 action = self.take_action(self.path, current)
                 print(" REACHED ")
                 return action
 
-            # self.open_set.remove(current)
-
             for neibour in current.neibours:
                 
-                # print(neibour.id)
                 temp_g_score = current.g_score + 1
                 
                 if temp_g_score < neibour.g_score:
@@ -183,12 +165,10 @@ class board():
                     neibour.g_score = temp_g_score
                     neibour.f_score = temp_g_score + self.h(neibour , self.end)
                     
-                    # if neibour not in self.open_set:
                     if neibour not in open_set_hash:
                         count += 1 
                         self.open_set.put((count , neibour))
                         open_set_hash.add(neibour)
-                        # self.open_set.append(neibour)
         
         print("CAN'T REACH")
         return False
@@ -199,6 +179,9 @@ class Agent():
 
         # any initialization code can go here
         self._client.set_game_tick_callback(self._on_game_tick)
+        self.board = Gameboard()
+
+
 
         loop = asyncio.get_event_loop()
         connection = loop.run_until_complete(self._client.connect())
@@ -206,6 +189,7 @@ class Agent():
             asyncio.ensure_future(self._client._handle_messages(connection)),
         ]
         loop.run_until_complete(asyncio.wait(tasks))
+
 
     # returns coordinates of the first bomb placed by a unit
     def _get_bomb_to_detonate(self, unit) -> Union[int, int] or None:
@@ -225,24 +209,43 @@ class Agent():
         my_units = game_state.get("agents").get(my_agent_id).get("unit_ids")
 
 
-        #TODO : a function which returns two values , a Action and which unit
-        #TODO : Graph position is different 
-
-        #TODO : need to add custom end path to route
-
         for unit_id in my_units:
-            # unit_id = 'd'
             
-            b = board(self._client._state , my_units , unit_id)
-            # unit_id = 'd'
-            #send each unit a random action
-            # action = random.choice(actions)
-            # print(self._client._state['unit_state']['d']['coordinates'])
-            # print(action)
-            # print(self._client._state['unit_state']['d']['coordinates'])
+            # unit_id = 'f'
+            
+            self.board.update_board(self._client._state)       
+            
+            ammo = []
+            bp   = None
 
-            action = b.path_finding()
-            del b
+            for i in range(15):
+                for j in range(15):
+                    if self.board.grid[i][j].obj_type == 'a':
+                        ammo = [i ,j]
+                        # ammo.append([i ,j])
+
+                    if self.board.grid[i][j].obj_type == 'bp':
+                        bp = [i , j]
+
+            if ammo:
+                # for  i in ammo:
+                print("AMMO PRESENT SIR !!! " , ammo)
+                action = self.board.path_finding(unit_id , ammo)
+
+            elif bp:
+                
+                print("POWER PRESENT SIR !!! " , bp)
+                action = self.board.path_finding(unit_id , bp)
+
+            else:
+                x = random.randint(0 , 14)
+                y = random.randint(0 , 14)
+                end = [x , y]
+                print("END SIR !!! " , end)
+
+                action = self.board.path_finding(unit_id , end)
+
+            # action = random.choice(actions)
             print(action)
        
             if action in ["up", "left", "right", "down"]:
